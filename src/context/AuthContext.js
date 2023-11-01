5ce986612b671f6a489fc22b2413a7c532b11da6
mariamtamerr <memariam5@outlook.com>
2023-11-01 23:16:10 +0200

    Firebase Imports In Auth Context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,40 @@
 import { createContext, useContext, useEffect, useState } from "react";
-
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword,
+    signInWithEmailAndPassword, 
+    signOut, 
+    onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = createContext()
 
 
 export function AuthContextProvider({children}) {
+    const [user, setUser] = useState({})
 
+    function signUp(email,password){
+        return createUserWithEmailAndPassword(email,password)
+    }
+
+    function login(email, password){
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logout(){
+        return signOut(auth)
+    }
+
+    // useEffect so it only mounts once
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=> {
+            setUser(currentUser)
+        })
+        return () => {
+            unsubscribe()
+        }
+    })
+   
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{user, signUp, login, logout}}>
             {children}
         </AuthContext.Provider>
 
