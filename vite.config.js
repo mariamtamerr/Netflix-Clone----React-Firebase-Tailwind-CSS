4a932ee95f0c3a6e63018be4e7cd9e96d2801afd
mariamtamerr <memariam5@outlook.com>
2023-11-07 02:23:30 +0200

    Deploy to github pages bc google cloud suspended it

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+  base:'/Netflix-Clone----React-Firebase-Tailwind-CSS/',
   plugins: [react()],
   build: { chunkSizeWarningLimit: 1600, }
 })
